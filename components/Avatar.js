e083ed3adaf24e325415bf3e6142b44707a7cf7e
itishprasad30 <itishprasad20@gmail.com>
2021-08-16 11:43:16 +0530

    image chageing

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,14 @@
+import Image from "next/image";
+
 const Avatar = ({ url }) => {
   function imageOnClick() {
     window.open("https://www.github.com/itishprasad30", "_blank").focus();
   }
   return (
-    <img
+    <Image
       loading="lazy"
+      height={40}
+      width={40}
       src={url}
       className="h-10 rounded-full cursor-pointer  transition duration-150 transform hover:scale-110"
       alt="profile pic"
